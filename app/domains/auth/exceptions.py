55817c549711852a66b36985ee12d3d97beb065f
Suah-Cho <sacho0929@gmail.com>
2025-05-11 13:46:44 +0900

    feat: #3 로그아웃 구현

--- app/domains/auth/exceptions.py
+++ app/domains/auth/exceptions.py
@@ -5,3 +5,7 @@ from app.common.exception import CustomException
 class PasswordNoMatch(CustomException):
     status_code: int = status.HTTP_401_UNAUTHORIZED
     message: str = "비밀번호가 일치하지 않습니다."
+
+class TokenNotFound(CustomException):
+    status_code: int = status.HTTP_401_UNAUTHORIZED
+    message: str = "토큰이 유효하지 않습니다. 로그인을 진행해주세요."
